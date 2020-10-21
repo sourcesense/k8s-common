6c0b704a6c0d5052970d56a1fad1e959b92bfe9d
Antonio Locane <a.locane@eco-mind.eu>
2020-10-21 11:15:38 +0200

    added helm.sh

---
+++ lib/helm.sh
@@ -0,0 +1,116 @@
+#!/usr/bin/env bash
+
+if type dep &>/dev/null ; then
+    dep include log2/shell-common:0.2.0 log
+else
+    include log2/shell-common lib/log.sh
+fi
+
+req helm jq yq
+
+chart_check(){
+
+    usage()
+    {
+        COLUMNS=$(tput cols) 
+        title="Analyse Helm chart to check for anomalies" 
+        printf "%*s\n" $(( ( ${#title} + COLUMNS ) / 2 )) "$title"
+        log ""
+        log "DESCRIPTION:"
+        log "simple script to check quality of Helm chart with respect to current release"
+        log ""
+        log "OPTIONS:"
+        log "  $(b -s) | $(b --show)"
+        log "      show chart content (uses $(i helm template))"
+        log "  $(b -d) | $(b --debug)"
+        log "      use debug option with helm"
+        log "  $(b -h) | $(b --help)"
+        log "      show this help"
+        log ""
+    }
+
+
+    SHOW_CONTENT=
+    DEBUG=
+
+    for i in "$@"
+    do
+        case $i in
+            -s | --show)
+                SHOW_CONTENT=true
+                shift
+                ;;	
+            -d | --debug)
+                DEBUG=true
+                shift
+                ;;
+            -h | --help )           
+                usage
+                exit
+                ;;
+            # * ) 
+            #     usage
+            #     exit 1
+        esac
+    done
+
+    valuesFile=$1
+    targetDir=$2
+
+    isDeployed() {
+        helm get values --namespace "$NAMESPACE" "$RELEASE_NAME" >/dev/null 2>&1
+    }
+
+    getVersion() {
+        helm get values -o json --namespace "$NAMESPACE" "$RELEASE_NAME" | jq ".$1.deployment.image.tag" -r
+    }
+
+    emit() {
+        local params=("$@")
+        printf "%b" "${params[@]}"
+    }
+
+    values() {
+        if [[ -n $(yq r - "kind" <"$valuesFile") ]] ; then
+            yq r - "spec.values" <"$valuesFile"
+        else
+            cat "$valuesFile"
+        fi
+    }
+
+    if [ -n "$DEBUG" ]; then
+        EXTRA_HELM_OPTIONS="--debug"
+    fi 
+
+    REQS=$targetDir/requirements.yaml
+    if [[ -f $REQS ]] ; then
+        start_log_line "Checking charts $(b "$targetDir") depends upon"
+        emit " ... found $(b "$(yq r "$REQS" -l 'dependencies')") dependencies"
+        for P in $(yq r "$REQS" --printMode p 'dependencies.*') ; do
+            DEP=$(yq r "$REQS" "$P.name")
+            VER=$(yq r "$REQS" "$P.version")
+            emit " ... checking $(b "$DEP") $(b "$VER")"
+            TGZ="$targetDir/charts/$DEP-$VER.tgz"
+            if [ -s "$TGZ" ]; then
+                end_log_line "(OK)"
+            else
+                end_log_line_err " FAIL!"
+                warn "Could not find archive for $(b "$DEP") $(b "$VER"), i.e., file $(b "$TGZ")." >&2 
+                whine "Please provide it by running $(b helm dep update "$targetDir") and re-run this script"
+            fi
+        done
+    fi
+    end_log_line " everything looks fine!"
+
+    start_log_line "Checking helm chart $(b "$targetDir") ..."
+    if helm lint "$targetDir" --values <(values); then
+        end_log_line " chart is fine"
+    else
+        whine "Could not use chart $(b "$targetDir"), helm found it to be bad"
+    fi
+
+    if [ -n "$SHOW_CONTENT" ]; then
+        log "Showing chart content as requested"
+        helm template "$targetDir" $EXTRA_HELM_OPTIONS --values <(values)
+    fi
+}
