5439cd5eb43c5c950d5c268b83f0ea3813e93f45
Antonio Fragola <a.fragola@eco-mind.eu>
2021-05-14 22:27:04 +0200

    added config for local e4t development

---
+++ lib/kube-config-local.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if type dep &>/dev/null ; then
+    dep include EcoMind/k8s-common kube-config
+else
+    include EcoMind/k8s-common lib/kube-config.sh
+fi
+
+set_aws_profile() {
+    local aws_profile_name="$1"
+    export AWS_PROFILE="${aws_profile_name}"
+    watch_if_exists "$HOME/.aws/config"
+    watch_if_exists "$HOME/.aws/credentials"
+}
+
